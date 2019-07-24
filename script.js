b88db8a4e8c150e64512877569b547e95c6b8204
kevinellis95 <47949419+kevinellis1995@users.noreply.github.com>
2019-07-24 18:44:15 -0400

    adding starting files

---
+++ script.js
@@ -0,0 +1,33 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var randomButton = document.getElementById("random");
+
+css.textContent = "linear-gradient( to right, " + color1.value + ", " + color2.value + ")";
+
+function setGradient() {
+    body.style.background = "linear-gradient( to right, " + color1.value + ", " + color2.value + ")";
+    css.textContent = body.style.background + ";";
+    color1
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+randomButton.addEventListener("click", changeColors);
+
+function changeColors() {
+    body.style.background = "linear-gradient( to right, " + getRandomColor() + ", " + getRandomColor() + ")";
+    css.textContent = body.style.background + ";";
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color; 
+}
